f2e916faf6d1d1f9f9f1e74227805cd569bf0f9e
fmelvold <78048522+fmelvold@users.noreply.github.com>
2023-04-19 16:35:48 +0200

    Fikset bilde
    Fikk bilde til å fungere

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,13 +1,13 @@
+import React from 'react';
+import logo from '../images/macslogo_white.png';
+
 export default function NavBar() {
   return (
     <nav className="nav-bar">
       <ul>
         <li>
           <a href="#" className="logo">
-            <img
-              src="./images/macslogo_white.png"
-              alt="Logo with the text Mac's GameHub"
-            />
+            <img src={logo} alt="Logo" />
           </a>
         </li>
         <li>
