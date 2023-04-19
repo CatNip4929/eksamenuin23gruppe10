eb4abf7db1244402495f1420a470710a483d1e54
CatNip4929 <111730636+CatNip4929@users.noreply.github.com>
2023-04-19 19:04:02 +0200

    flytting av filer...
    flyttet components til pages...

--- Sanity/dimgray-cobra/schemas/games.js
+++ Sanity/dimgray-cobra/schemas/games.js
@@ -1,35 +1,75 @@
 export default {
     name: 'games',
     type: 'document',
-    title: 'spill-navn',
+    title: 'Spill',
     fields: [
         {
-            name: 'games_title',
+            name: 'game_title',
             type: 'string',
-            title: 'Spill-navn'
+            title: 'Spill'
         },
         {
             name: 'slug',
             type: 'slug',
             title: 'URL-tittel',
             options: {
-                source: 'games_title',
+                source: 'game_title',
                 slugify: input => input.toLowerCase().replace(/\s+/g, '-').slice(0,150)
             }
         },
         {
-            name: 'games_rating',
+            name: 'game_rating',
             type: 'number',
-            title: 'vurdering'
+            title: 'Rating'
+        },
+        {
+            name: 'game_plot',
+            type: 'text',
+            title: 'Oppsumering'
+        },
+        {
+            name: 'game_tags',
+            type: 'string',
+            title: 'Stikkord'
+        },
+        {
+            name: 'game_developers',
+            type: 'string',
+            title: 'Utviklere'
+        },
+        {
+            name: 'game_publisher',
+            type: 'string',
+            title: 'Utgiver'
+        },
+        {
+            name: 'game_release',
+            type: 'date',
+            title: 'Utgivelsesår'
+        },
+        {
+            name: 'game_platforms',
+            type: 'string',
+            title: 'Plattformer'
+        },
+        {
+            name: 'game_stores',
+            type: 'string',
+            title: 'Kjøpsmuligheter'
+        },
+        {
+            name: 'game_time',
+            type: 'string',
+            title: 'Timer-Spilt'
         },
         {
             name: 'game_image',
             type: 'image',
-            title: 'game_poster'
+            title: 'Bilde'
         },
         {
             name: 'game_category',
-            title: 'spillkategori',
+            title: 'Spillkategori',
             type: 'reference',
             to: [{type: 'categories'}]
         }
