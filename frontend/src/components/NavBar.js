12bf7a5ba3e2b1fa42fde7fd10ff5bd6766c5e16
snilseen <nilzen@live.com>
2023-04-19 16:24:43 +0200

    Nav Bar Site
    laget førsteutkast navbar.

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,8 +1,25 @@
-
-//
-
-
-export default function NavBar () {
-
-  return ()
+export default function NavBar() {
+  return (
+    <nav className="nav-bar">
+      <ul>
+        <li>
+          <a href="#" className="logo">
+            <img
+              src="./images/macslogo_white.png"
+              alt="Logo with the text Mac's GameHub"
+            />
+          </a>
+        </li>
+        <li>
+          <a href="#">My Games</a>
+        </li>
+        <li>
+          <a href="#">My Favourites</a>
+        </li>
+        <li>
+          <a href="#">Game Shop</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
