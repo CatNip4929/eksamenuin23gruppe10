21c16d09ba175dd758c18d9cdfb283025924806b
snilseen <nilzen@live.com>
2023-05-05 17:08:01 +0200

    latest version av navbaren
    siste versjon av navbaren

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,22 +1,16 @@
-import React from 'react';
-import logo from '../images/macslogo_white.png';
+import React from "react";
+import logo from "../images/macslogo_white.png";
 
 export default function NavBar() {
   return (
     <nav className="nav-bar">
+      <a href="#">
+        <img src={logo} alt="Logo" className="logo" />
+      </a>
       <ul>
         <li>
-          <a href="#" className="logo">
-            <img src={logo} alt="Logo" />
-          </a>
-        </li>
-        <li>
           <a href="#">My Games</a>
-        </li>
-        <li>
           <a href="#">My Favourites</a>
-        </li>
-        <li>
           <a href="#">Game Shop</a>
         </li>
       </ul>
