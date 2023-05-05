f31365d60e25b1f0f231024ec676885450dddf4f
fmelvold <78048522+fmelvold@users.noreply.github.com>
2023-05-05 21:00:35 +0200

    Api og routing
    Lagt inn api for å liste opp 3 spill i dashboard og 10 for gameshop. Har også fikset routing for dashboard og gameshop

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,24 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
+import Dashboard from './components/Dashboard';
 import "./css/main.css";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+import GameShop from './components/GameShop';
+
+
 function App() {
-  return (
-    <div className="App">
-      <NavBar />
-    </div>
-  );
-}
+      return (
+        <Router>
+          <div className="App">
+            <NavBar />
+            <Routes>
+              <Route index element={<Dashboard />} />
+              <Route path="/gameshop" element={<GameShop />} />
+            </Routes>
+          </div>
+        </Router>
+      );
+    }
+    
 
 export default App;
--- frontend/src/components/GameShop.js
+++ frontend/src/components/GameShop.js
@@ -1,3 +1,32 @@
-export default function GameShop () {
-  return ()
+import React, { useState, useEffect } from 'react';
+
+export default function GameShop() {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    const fetchGames = async () => {
+      const response = await fetch(`https://api.rawg.io/api/games?ordering=-released&key=37183cfb9c70422587800abe07c5aebc&page_size=100`);
+      const data = await response.json();
+      const imageGames = data.results.filter((game) => game.background_image !== null);
+      const gameLimitations = imageGames.slice(0, 10);
+      setGames(gameLimitations);
+    };
+
+    fetchGames();
+  }, []);
+
+  return (
+    <div>
+      <h1>GameShop</h1>
+      <div className="games-container">
+      {games.map((game, index) => (
+          <div key={index}>
+            <img src={game.background_image} style={{maxWidth: "200px"}} alt={game.name} />
+            <h2>{game.name}</h2>
+            <p>{game.genres.map((genre) => genre.name).join(", ")}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
