fda6c263056a37597b2e57d8b19f788c6eb98b6c
snilseen <nilzen@live.com>
2023-05-12 17:02:38 +0200

    foreløpig greier
    kjør

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,27 +1,31 @@
 import React from "react";
 import logo from "../images/macslogo_white.png";
-import { Link, Outlet } from "react-router-dom"
+import { Link, Outlet } from "react-router-dom";
 
 export default function NavBar() {
   return (
     <>
-    <nav className="nav-bar">
-      <Link to="/">
-        <img src={logo} alt="Logo" className="logo" />
+      <nav className="nav-bar">
+        <Link to="/">
+          <img src={logo} alt="Logo" className="logo" />
         </Link>
-      <ul>
-        <li>
-        <Link to="/mygames">My Games</Link>        
-        </li>
-        <li>
-        <Link to="/myfavorites">My Favorites</Link>        
-        </li>
-        <li>
-        <Link to="/gameshop">Gameshop</Link>        
-        </li>
-      </ul>
-    </nav>
-     <Outlet />
-     </>
+        <ul>
+          <li>
+            <Link to="/mygames" activeClassName="active">
+              My Games
+            </Link>
+
+            <Link to="/myfavorites" activeClassName="active">
+              My Favorites
+            </Link>
+
+            <Link to="/gameshop" activeClassName="active">
+              Gameshop
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
+    </>
   );
 }
