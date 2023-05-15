eb0b02bf4b43b9309eeb6d22605c10680971308a
snilseen <nilzen@live.com>
2023-05-15 15:18:49 +0200

    Litt styling på layout dashboard
    Laget grid og sånt for dashboard

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,21 +1,22 @@
 import FavoriteGames from "./FavoriteGames";
-import GameCard from "./GameCard"
-import GameShop from "./GameShop"
-import MyGames from "./pages/MyGames"
+import GameCard from "./GameCard";
+import GameShop from "./GameShop";
+import MyGames from "./pages/MyGames";
+import "../css/main.css";
 
-export default function Dashboard ({games, gameinfo, user }) {
+export default function Dashboard({ games, gameinfo, user }) {
   return (
     <>
-      <main>
-        <article>
-      <GameShop gamesToShow={3}/>
-      </article>
-      <article>
-      <MyGames games={games} gameinfo={gameinfo}/>
-      </article>
-      <article>
-        <FavoriteGames user={user}/>
-      </article>
+      <main className="container">
+        <article className="game-shop">
+          <GameShop gamesToShow={3} />
+        </article>
+        <article className="my-games">
+          <MyGames games={games} gameinfo={gameinfo} />
+        </article>
+        <article className="favorite-games">
+          <FavoriteGames user={user} />
+        </article>
       </main>
     </>
   );
--- frontend/src/components/FavoriteGames.js
+++ frontend/src/components/FavoriteGames.js
@@ -1,23 +1,24 @@
-import { useEffect, useState } from "react"
-import { fetchUserLibrary } from "../lib/sanity/userServices"
-import GameCard from "./GameCard"
+import { useEffect, useState } from "react";
+import { fetchUserLibrary } from "../lib/sanity/userServices";
+import GameCard from "./GameCard";
 
-
-export default function ({user}) {
-    const [userLib, setUserLibrary] = useState()
-    //fetch for alle spill
+export default function ({ user }) {
+  const [userLib, setUserLibrary] = useState();
+  //fetch for alle spill
   const getUserLib = async () => {
-    const data = await fetchUserLibrary(user.user_mail)
-    setUserLibrary(data)
-    }
+    const data = await fetchUserLibrary(user.user_mail);
+    setUserLibrary(data);
+  };
 
-    useEffect (() => {
-       getUserLib(user) 
-    }, [user] )
-    return(
-        <>
-      <h1>My favorites</h1>
-      {userLib?.map((p,i) =>  (console.log(p[0]) ))}
-        </>
-    )
+  useEffect(() => {
+    getUserLib(user);
+  }, [user]);
+  return (
+    <>
+      <div className="favorites">
+        <h1 className="center-title">My favorites</h1>
+        {userLib?.map((p, i) => console.log(p[0]))}
+      </div>
+    </>
+  );
 }
